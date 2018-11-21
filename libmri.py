08084a0b1f6b47a8bc2031ec5f197d437d25f9ec
Edwin Dalmaijer <edwin.dalmaijer@gmail.com>
2018-11-21 18:20:14 +0000

    Updated libmri based on MRI and MEG testing seshs

--- libmri.py
+++ libmri.py
@@ -13,10 +13,13 @@
 # Author: Edwin Dalmaijer
 # Email: Edwin.Dalmaijer@mrc-cbu.cam.ac.uk
 # Date: 2018-10-24
-# Last update: 2018-10-24
+# Last update: 2018-11-21
 
+import copy
 import time
+
 import nidaqmx
+from nidaqmx.constants import LineGrouping
 
 
 class MRITriggerBox:
@@ -37,22 +40,11 @@ class MRITriggerBox:
         # Channel for scanner pulse.
         self._scan_chan = "port0/line0"
         
-        # Create a dict with all short-hand button names and associated ports.
+        # Create a list with all short-hand button names.
         self._button_list = ["B1", "B2", "B3", "B4"]
-        self._buttons = { \
-            "B1":   "port0/line1", \
-            "B2":   "port0/line2", \
-            "B3":   "port0/line3", \
-            "B4":   "port0/line4", \
-            }
-        
-        # Create a dict with all the trigger names and associated ports.
-        n_trigger_channels = 8
-        self._trigger_list = range(n_trigger_channels)
-        self._triggers = {}
-        for i in self._trigger_list:
-            self._triggers[i] = "port2/line%d" % (i)
-        
+        # Set all the channel names for the buttons.
+        self._button_channels = "port0/line1:4"
+                
         # INITIALISE
         print("\nInitialising connection to the NI box...")
         # Initialise the system.
@@ -67,7 +59,6 @@ class MRITriggerBox:
         
         print("\nConnection established with '%s'!" % (self._dev_name))
         print("\tDevice: %s" % (self._dev))
-        print("\tDriver version: %s" % (system.driver_version))
         print("\nChannel details:")
         print("Scanner pulse: %s" % (self._scan_chan))
         print("Buttons: %s" % (self._buttons))
@@ -85,49 +76,149 @@ class MRITriggerBox:
         Returns
         
         button_list, state  -   button_list is a list of all button names, and
-                                state is a list of the associated values.
+                                state is a list of the associated Booleans.
         """
-        
-        if button_list is None:
-            button_list = self._button_list[:]
         
         # Create a new Task to listen in on the button channels.
         with nidaqmx.Task() as task:
             # Add the digital input (di) channels.
-            for butt in button_list:
-                task.di_channels.add_di_chan("%s/%s:1" % \
-                    (self._dev_name, self._buttons[butt]))
+            task.di_channels.add_di_chan( \
+                "%s/%s" % (self._dev, self._button_channels), \
+                line_grouping=LineGrouping.CHAN_PER_LINE)
             # Get a single sample from the digital input channels.
             state = task.read(number_of_samples_per_channel=1, \
-                timeout=1.0)
+                timeout=0.001)
         
-        return button_list, state
+        # Unwrap state, which is a list of lists of samples.
+        # Example: [[False], [False], [False], [False], [False]]
+        # We want instead: [False, False, False, False, False]
+        for i, b in enumerate(state):
+            state[i] = b[0]
+
+        # Select all buttons if no specific ones are requested.
+        if button_list is None:
+            return copy.deepcopy(self._button_list), state
+
+        # Return only the requested buttons.
+        else:
+            l = []
+            for b in button_list:
+                if b not in self._button_list:
+                    raise Exception("ERROR: Unknown button '%s'; available buttons: %s" \
+                        % (b, self._button_list))
+                i = self._button_list.index(b)
+                l.append(state[i])
+            return button_list, l
     
     
     def wait_for_sync(self, timeout=10.0):
         
+        """
+        """
+        
         # Get the starting time.
         t0 = time.time()
+
         # Create a new Task to listen in on the button channels.
         with nidaqmx.Task() as task:
+            # Add the digital input (di) channels.
+            task.di_channels.add_di_chan("%s/%s" % \
+                (self._dev_name, self._scan_chan))
+
             # Loop until a signal or a timeout happens.
             triggered = False
             timed_out = False
             while not triggered and not timed_out:
-                # Add the digital input (di) channels.
-                task.di_channels.add_di_chan("%s/%s:1" % \
-                    (self._dev_name, self._scan_chan))
                 # Get a single sample from the digital input channels.
                 state = task.read(number_of_samples_per_channel=1, timeout=1.0)
-                t = time.time()
-                # Check the sample.
-                if state:
+                t1 = time.time()
+                # Check the sample (turns False when triggered).
+                if not state[0]:
                     triggered = True
+                    break
                 # Check the time.
-                if t - t0 > timeout:
+                if t1 - t0 > timeout:
                     timed_out = True
         
-        return t, triggered, timed_out
+        return t1, triggered, timed_out
+    
+    
+    def wait_for_button_press(self, allowed=None, timeout=None):
+        
+        """Waits for a button press.
+        
+        Keyword Arguments
+        
+        allowed             -   List of strings with allowed button names, or
+                                None to allow all buttons. Default = None
+        
+        timeout             -   Float or int that indicates the timeout in
+                                seconds. If no button is pressed within the
+                                timeout, this function will return. The
+                                timeout can be None, meaning no timeout will
+                                occur. Default = None
+        
+        Returns
+
+        button, time        -   button is a string that indicates the pressed
+                                button's name (only the first-pressed button
+                                is counted), or None if no button was pressed
+                                before a timeout occured.
+                                time is a float value that reflects the time
+                                in seconds at the time the button press was
+                                detected.
+        """
+        
+        # Get the indices of the allowed buttons.
+        if allowed is not None:
+            allow = []
+            for b in allowed:
+                if b not in self._button_list:
+                    raise Exception("ERROR: Unknown button '%s'; available buttons: %s" \
+                        % (b, self._button_list))
+                allow.append(self._button_list.index(b))
+        else:
+            allow = range(len(self._button_list))
+
+        # Get the starting time.
+        t0 = time.time()
+        t1 = time.time()
+        
+        # Create a new Task to listen in on the button channels. Using a with
+        # statement will automatically close the Task if an error happens
+        # during execution, leaving the NI box in a better state.
+        with nidaqmx.Task() as task:
+
+            # Add the digital input (di) channels.
+            task.di_channels.add_di_chan( \
+                "%s/%s" % (self._dev, self._button_channels), \
+                line_grouping=LineGrouping.CHAN_PER_LINE)
+            # Start the task (this will reduce timing inefficience when
+            # calling the task.read function).
+            task.start()
+
+            # Run until a timeout or a button press occurs.
+            button = None
+            pressed = False
+            while not pressed and t1 - t0 < timeout:
+
+                # Get a single sample from the digital input channels.
+                state = task.read(number_of_samples_per_channel=1, \
+                    timeout=0.001)
+                # Get a timestamp for the sample.
+                t1 = time.time()
+
+                # Check whether any of the allowed buttons were pressed.
+                for i in allow:
+                    if state[i][0]:
+                        pressed = True
+                        button = self._button_list[i]
+                        break
+            
+            # Stop the task.
+            task.stop()
+                
+        return button, t1
         
         
         
